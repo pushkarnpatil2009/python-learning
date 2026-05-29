f25ce1db9034894a23f6be82f98ab82db14bb7ec
pushkarnpatil2009 <pushkarnpatil2009@gmail.com>
2026-05-29 13:09:19 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,6 +6,6 @@ print("Subtraction =", num1 - num2)
 print("Multiplication =", num1 * num2)
 
 if num2 != 0:
-print("Division =", num1 / num2)
+    print("Division =", num1 / num2)
 else:
-print("Cannot divide by zero")
+    print("Cannot divide by zero")
